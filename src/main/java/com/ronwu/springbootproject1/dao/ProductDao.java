de0916eed385e3cdf5279264ef69f33f560a2f37
wujungen <wujungen1008@gmail.com>
2024-01-27 20:40:36 +0800

    function - create product

--- src/main/java/com/ronwu/springbootproject1/dao/ProductDao.java
+++ src/main/java/com/ronwu/springbootproject1/dao/ProductDao.java
@@ -1,7 +1,9 @@
 package com.ronwu.springbootproject1.dao;
 
+import com.ronwu.springbootproject1.dto.ProductRequest;
 import com.ronwu.springbootproject1.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/ronwu/springbootproject1/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ronwu/springbootproject1/service/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ronwu.springbootproject1.service.impl;
 
 import com.ronwu.springbootproject1.dao.ProductDao;
+import com.ronwu.springbootproject1.dto.ProductRequest;
 import com.ronwu.springbootproject1.model.Product;
 import com.ronwu.springbootproject1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +16,9 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Product getProductById(Integer productId) {
         return productDao.getProductById(productId);
+    }
+
+    public Integer createProduct(ProductRequest productRequest){
+        return productDao.createProduct(productRequest);
     }
 }
