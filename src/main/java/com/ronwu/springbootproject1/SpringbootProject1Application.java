2bf2368fddfd6c031b696b4946ef5f71b7396646
wujungen <wujungen1008@gmail.com>
2024-01-27 14:38:57 +0800

    initialize springboot project

---
+++ src/main/java/com/ronwu/springbootproject1/SpringbootProject1Application.java
@@ -0,0 +1,13 @@
+package com.ronwu.springbootproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProject1Application.class, args);
+    }
+
+}
