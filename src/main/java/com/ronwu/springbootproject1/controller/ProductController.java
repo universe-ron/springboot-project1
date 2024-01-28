1dfadb1ff896d1050e14614d0fa74ac620b88319
wujungen <wujungen1008@gmail.com>
2024-01-28 22:07:04 +0800

    function - product query list (paging)

--- src/main/java/com/ronwu/springbootproject1/controller/ProductController.java
+++ src/main/java/com/ronwu/springbootproject1/controller/ProductController.java
@@ -6,13 +6,16 @@ import com.ronwu.springbootproject1.dto.ProductRequest;
 import com.ronwu.springbootproject1.model.Product;
 import com.ronwu.springbootproject1.service.ProductService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Validated
 @RestController
 public class ProductController {
 
@@ -26,13 +29,19 @@ public class ProductController {
             @RequestParam(required = false) String search,
             //排序Sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+
+            //Pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0)Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
     ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
         return ResponseEntity.status(HttpStatus.OK).body(productList);
--- src/main/java/com/ronwu/springbootproject1/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/ronwu/springbootproject1/dao/impl/ProductDaoImpl.java
@@ -30,6 +30,7 @@ public class ProductDaoImpl implements ProductDao {
                 "last_modified_date " +
                 "FROM product WHERE 1=1";
         Map<String, Object> map = new HashMap<>();
+        //查詢條件
          if(productQueryParams.getCategory() != null){
              sql = sql + " AND category = :category";
              map.put("category",productQueryParams.getCategory().name());
@@ -38,8 +39,12 @@ public class ProductDaoImpl implements ProductDao {
              sql = sql + " AND product_name LIKE :search";
              map.put("search","%"+productQueryParams.getSearch()+"%");
          }
+        //排序
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
-
+        //分頁
+         sql = sql + " LIMIT :limit OFFSET :offset";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
         return productList;
